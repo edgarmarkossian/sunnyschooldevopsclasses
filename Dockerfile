2737dc5ab5c9d8b5c5f6cf6e693449095c517c7b
edomarkosyan <edomarkosyan@gmail.com>
2024-05-20 10:48:52 +0400

    Homewrok done

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM nginx:latest
-RUN apt-get update \ 
-    && apt-get install -y \
-    vim \
-    mc \
-    curl
-COPY index.html /usr/share/nginx/html/
---
+++ scripts.sh
@@ -0,0 +1,3 @@
+kubectl set image deployment/my-deployment my-container=nginx:1.19.3 #imageSet
+
+kubectl rollout undo deployment/my-deployment #rolloutUndo
